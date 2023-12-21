245ba24678af9bab20d6a0b06c2dd8de13e54555
Luiz Fernando <lfrprazeres@gmail.com>
2023-12-21 13:23:52 -0300

    2.2 - Introduzindo Efffects

--- src/store/reducers/carrinho/sagas.ts
+++ src/store/reducers/carrinho/sagas.ts
@@ -1,10 +1,14 @@
-function* escutando() {
+import { take } from "redux-saga/effects";
 
+function* escutando() {
+  console.log("estou escutando!");
 }
 
-
 function* carrinhoSaga() {
-
+  while (true) {
+    yield take("carrinho/iniciarBuscaCarrinho");
+    yield escutando();
+  }
 }
 
 export default carrinhoSaga;
