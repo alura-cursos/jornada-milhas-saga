0194b1f1e052acd899c8b6d97284df17cc52d2c6
Luiz Fernando <lfrprazeres@gmail.com>
2023-12-21 13:46:01 -0300

    3.1 - Utilizando o payload

--- src/store/reducers/carrinho/sagas.ts
+++ src/store/reducers/carrinho/sagas.ts
@@ -1,12 +1,26 @@
 import { takeLatest } from "redux-saga/effects";
 import { iniciarBuscaCarrinho } from './index';
+import { PayloadAction } from '@reduxjs/toolkit';
+import { BuscaCarrinho } from 'src/types/carrinho';
 
-function* escutando() {
-  console.log("estou escutando usando o type!");
+function* buscarTodos({ payload }: PayloadAction<BuscaCarrinho>) {
+  const { buscarCarro, buscarHotel, buscarVoo } = payload;
+
+  if (buscarHotel) {
+    console.log('buscando hotel');
+  }
+
+  if (buscarVoo) {
+    console.log('buscando passagens');
+  }
+
+  if (buscarCarro) {
+    console.log('buscando carro');
+  }
 }
 
 function* carrinhoSaga() {
-  yield takeLatest(iniciarBuscaCarrinho.type, escutando);
+  yield takeLatest(iniciarBuscaCarrinho.type, buscarTodos);
 }
 
 export default carrinhoSaga;
