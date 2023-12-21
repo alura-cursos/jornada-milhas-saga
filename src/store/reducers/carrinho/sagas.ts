1f84c54e302c9b6ec842b707d91b9988ccf07bac
Luiz Fernando <lfrprazeres@gmail.com>
2023-12-21 14:09:46 -0300

    3.3 - Reservando passagens

--- src/store/reducers/carrinho/sagas.ts
+++ src/store/reducers/carrinho/sagas.ts
@@ -1,7 +1,8 @@
-import { put, takeLatest } from "redux-saga/effects";
-import { comecarBusca, iniciarBuscaCarrinho } from './index';
+import { delay, put, takeLatest } from "redux-saga/effects";
+import { comecarBusca, iniciarBuscaCarrinho, mudarStatusCarrinho, resetarCarrinho } from './index';
 import { PayloadAction } from '@reduxjs/toolkit';
-import { BuscaCarrinho, TodosOsStatus } from 'src/types/carrinho';
+import { BuscaCarrinho, StatusCarrinho, TodosOsStatus } from 'src/types/carrinho';
+import { baseUrl } from 'src/config/api';
 
 function* buscarTodos({ payload }: PayloadAction<BuscaCarrinho>) {
   const { buscarCarro, buscarHotel, buscarVoo } = payload;
@@ -19,6 +20,12 @@ function* buscarTodos({ payload }: PayloadAction<BuscaCarrinho>) {
 
 function* reservarVoo() {
   yield put(comecarBusca(TodosOsStatus.statusPassagens));
+  const resultado: Response = yield fetch(`${baseUrl}reservar`);
+  const deuErro = resultado.status === 404;
+  const novoStatus = deuErro ? StatusCarrinho.erro : StatusCarrinho.sucesso;
+  yield put(mudarStatusCarrinho({ [TodosOsStatus.statusPassagens]: novoStatus }));
+  yield delay(5000);
+  yield put(resetarCarrinho());
 }
 
 function* carrinhoSaga() {
